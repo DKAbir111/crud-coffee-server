1615754e0338dcdbe3dab8220c3f59b554215d22
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2024-11-27 00:20:44 +0600

    create a post request and receive data from client side

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ const port = process.env.PORT || 5001
 app.get('/', (req, res) => {
     res.send("Basic server setup done")
 })
+app.post('/addcoffee', (req, res) => {
+    const newCoffee = req.body;
+    res.send(newCoffee)
+    console.log(newCoffee)
+})
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
